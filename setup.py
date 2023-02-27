3c79012f102653ae77f2bc951c3cb405cdc8bf54
Yan Antônio <102044040+Yan908@users.noreply.github.com>
2023-02-27 11:31:28 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import io
 from setuptools import setup
 
+here = os.path.abspath(os.path.dirname(__file__))
+
 with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = '\n' + f.read()
     
