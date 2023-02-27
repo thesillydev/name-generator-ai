3c565f4d5914f2c6014f7fd8a63a2d9c4edb30bc
Yan Antônio <102044040+Yan908@users.noreply.github.com>
2023-02-27 11:42:31 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 from requests import get
 from setuptools import setup
-
-here = get('https://raw.githubusercontent.com/Yan908/name-generator-ai/main/README.md').text
-
-with open(here) as f:
-    long_description = '\n' + f.read()
     
 setup(
     name='name-generator',
     version='1.0.0',
     description='An simple open source name generator.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     author='A Weirdo Dev',
     python_requires='>=3.6.0',
     py_modules=['name_creator'],
