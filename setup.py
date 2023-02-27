7d17a9c39044697c04333c2f8e91f80919c70103
Yan Antônio <102044040+Yan908@users.noreply.github.com>
2023-02-27 11:56:44 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from requests import get
-from setuptools import setup
+from setuptools import setup, find_packages
     
 setup(
     name='name-generator',
@@ -7,7 +7,7 @@ setup(
     description='An simple open source name generator.',
     author='A Weirdo Dev',
     python_requires='>=3.6.0',
-    packages=['name_creator'],
-    install_requires=['tensorflow', 'numpy'],
+    packages=find_packages(),
+    install_requires=['name-generator-ai @ https://github.com/Yan908/name-generator-ai.git'],
     license='MIT'
 )
